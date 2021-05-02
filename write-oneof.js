26e330abef786c9a5dbd8474e8ddda3feb211e68
Kenneth Lum <kenneth_lum@apple.com>
2021-05-02 02:14:33 -0700

    ok

--- write-oneof.js
+++ write-oneof.js
@@ -3,15 +3,15 @@
 const fs = require("fs");
 const protobuf = require("protobufjs");
 
-protobuf.load("awesome-several.proto", function (err, root) {
+protobuf.load("awesome3.proto", function (err, root) {
   if (err) throw err;
 
   const AwesomeMessage = root.lookupType("awesomepackage.GenericMessage");
 
   //   const payload = { messageNumber: { a: 150 }};
   const payloads = [
-    { nameMessage: { name: "Peter" } },
-    { numberMessage: { num: 150 } },
+    { nameMessage: { awesomeString: "Peter" } },
+    { messageNumber: { a: 150 } },
   ];
   payloads.forEach((payload) => {
     console.log("payload", payload);
